32babc203aa3e29c9a336fb631794f7a5a87a22c
BenlDongA <nguyencuongbenl@gmail.com>
2026-03-10 20:41:41 +0700

    fixxxx

--- controllers/userController.js
+++ controllers/userController.js
@@ -118,6 +118,8 @@ exports.updateUser = async (req,res)=>{
 
     }catch(err){
 
+        console.log(err)
+
         res.status(500).json(err)
 
     }
--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,26 +1,27 @@
 const jwt = require("jsonwebtoken")
 
-const verifyToken = (req,res,next)=>{
+module.exports = function(req,res,next){
 
-    const token = req.headers["authorization"]
+    const authHeader = req.headers.authorization
 
-    if(!token){
-        return res.json({message:"No token"})
+    if(!authHeader){
+        return res.status(401).json({message:"No token"})
     }
+
+    const token = authHeader.split(" ")[1]
 
     try{
 
         const decoded = jwt.verify(token,"SECRET_KEY")
 
-        req.userId = decoded.id
+        req.user = decoded
 
         next()
 
     }catch(err){
 
-        res.json({message:"Invalid token"})
+        return res.status(403).json({message:"Invalid token"})
+
     }
 
 }
-
-module.exports = verifyToken
